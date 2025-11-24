7469510afd82c69836a0ee2b8766230e00d2dd75
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-24 12:41:19 +0800

    wait more if previous fetch has failed

--- bestdori-leaderboard/src/bestdori/index.ts
+++ bestdori-leaderboard/src/bestdori/index.ts
@@ -1,30 +1,35 @@
 import { limitFunction } from "p-limit";
 
+let previousFailCounter = 0;
 export const bestdori = limitFunction(
-	async <T>(path: string, query: Record<string, string>): Promise<T> => {
-		const url = new URL(path, "https://bestdori.com/");
-		url.search = new URLSearchParams(query).toString();
+  async <T>(path: string, query: Record<string, string>): Promise<T> => {
+    const url = new URL(path, "https://bestdori.com/");
+    url.search = new URLSearchParams(query).toString();
 
-		let failCounter = 0;
-		while (true) {
-			const response = await fetch(url);
-			const contentType = response.headers.get("content-type") ?? "";
-			if (!contentType.startsWith("application/json")) {
-				failCounter += 1;
-				if (failCounter >= 5)
-					throw new Error(`Error while fetching ${url.href}`);
+    let failCounter = 0;
+    await sleep(previousFailCounter * 1_000 + 3_000 + Math.random() * 1_000);
+    while (true) {
+      const response = await fetch(url);
+      const contentType = response.headers.get("content-type") ?? "";
+      if (!response.ok || !contentType.startsWith("application/json")) {
+        failCounter += 1;
+        if (failCounter >= 5) {
+          throw new Error(`Error while fetching ${url.href}`);
+        }
 
-				await new Promise((resolve) =>
-					setTimeout(
-						resolve,
-						Math.pow(2, failCounter) * 1000 + Math.random() * 1500,
-					),
-				);
-				continue;
-			}
+        await sleep(
+          Math.pow(2, failCounter) * 1_000 +
+            Math.random() * failCounter * 1_000,
+        );
+        continue;
+      }
 
-			return response.json();
-		}
-	},
-	{ concurrency: 1 },
+      previousFailCounter = failCounter;
+      return response.json();
+    }
+  },
+  { concurrency: 1 },
 );
+
+const sleep = (duration: number) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
