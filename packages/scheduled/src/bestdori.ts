8af11eeb7843f8d38c8b65d62e7cf630bd3259d7
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-25 21:50:59 +0800

    propagate retrying fetch to trigger.dev

--- packages/scheduled/src/bestdori.ts
+++ packages/scheduled/src/bestdori.ts
@@ -1,4 +1,4 @@
-import { logger, wait } from "@trigger.dev/sdk";
+import { wait } from "@trigger.dev/sdk";
 import { limitFunction } from "p-limit";
 
 export const bestdori = limitFunction(
@@ -6,35 +6,13 @@ export const bestdori = limitFunction(
 		const url = new URL(path, "https://bestdori.com/");
 		url.search = new URLSearchParams(query).toString();
 
-		await sleep();
-		let failCounter = 0;
-		while (true) {
-			const response = await fetch(url);
-			const contentType = response.headers.get("content-type") ?? "";
-			if (!response.ok || !contentType.startsWith("application/json")) {
-				failCounter += 1;
-				if (failCounter > 6) {
-					throw new Error(`Error while fetching ${url.href}`);
-				}
+		await wait.for({ seconds: 5 + Math.random() });
+		const response = await fetch(url);
+		const contentType = response.headers.get("content-type") ?? "";
+		if (!response.ok || !contentType.startsWith("application/json"))
+			throw new Error(`Error fetching ${url.href} (${response.status})`);
 
-				const content = await response.text();
-				const status = response.status;
-				logger.error("Error fetching bestdori", {
-					counter: failCounter,
-					url: url.href,
-					status,
-					content,
-				});
-
-				await sleep(Math.pow(2, failCounter));
-				continue;
-			}
-
-			return response.json();
-		}
+		return response.json();
 	},
-	{ concurrency: 2 },
+	{ concurrency: 3 },
 );
-
-const sleep = (duration?: number) =>
-	wait.for({ seconds: 5 + (duration ?? 0) + Math.random() });
