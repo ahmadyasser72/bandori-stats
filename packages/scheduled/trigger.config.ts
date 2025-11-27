467157a06443ec5fe54c3737ac711955369b1eb3
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-27 13:43:31 +0800

    use bun runtime

--- packages/scheduled/trigger.config.ts
+++ packages/scheduled/trigger.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
 	project: "proj_whqjzbgkgslcubigolcl",
-	runtime: "node",
+	runtime: "bun",
 	logLevel: "debug",
 	maxDuration: 3600,
 	machine: "micro",
