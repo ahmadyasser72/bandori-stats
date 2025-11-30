ef344d608621a611e1a61a781ef2f8e15e4c108c
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-30 17:46:13 +0800

    format stat numbers with thousand separators

--- site/src/lib/math.ts
+++ site/src/lib/math.ts
@@ -1,2 +1,5 @@
+const numberFormatter = Intl.NumberFormat("en-US");
+export const formatNumber = (n: number) => numberFormatter.format(n);
+
 export const sum = (values: number[]) =>
 	values.reduce((acc, next) => acc + next, 0);
