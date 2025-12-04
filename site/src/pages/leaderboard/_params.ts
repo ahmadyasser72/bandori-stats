83b4f7704bfff02e56b6ef3949866c4c18cbff71
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-12-04 20:32:18 +0800

    implement sorting by last updated

--- site/src/pages/leaderboard/_params.ts
+++ site/src/pages/leaderboard/_params.ts
@@ -1,9 +1,20 @@
 import { STAT_COLUMNS } from "@bandori-stats/database/constants";
 import z from "zod";
 
-export const PAGE = z.coerce.number().positive().catch(1);
+const schema = {
+	page: z.coerce.number().positive().catch(1),
+	rank_by: z
+		.array(z.enum(STAT_COLUMNS))
+		.nonempty()
+		.catch(() => [...STAT_COLUMNS]),
+	sort_latest: z
+		.literal("on")
+		.transform((it) => it === "on")
+		.catch(false),
+};
 
-export const RANK_BY = z
-	.array(z.enum(STAT_COLUMNS))
-	.nonempty()
-	.catch(() => [...STAT_COLUMNS]);
+export const parseSearchParams = (s: URLSearchParams) => ({
+	page: schema.page.parse(s.get("page")),
+	rank_by: schema.rank_by.parse(s.getAll("rank_by")),
+	sort_latest: schema.sort_latest.parse(s.get("sort_latest")),
+});
