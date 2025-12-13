d1e2a8bdfa5227d3d095b228fdc2e671fce9864e
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-12-13 22:03:37 +0800

    update stats dialog content

--- site/src/lib/math.ts
+++ site/src/lib/math.ts
@@ -1,5 +1,12 @@
 const numberFormatter = Intl.NumberFormat("en-US");
-export const formatNumber = (n: number) => numberFormatter.format(n);
+const numberFormatterCompact = Intl.NumberFormat("en-US", {
+	notation: "compact",
+});
+export const formatNumber = (n: number, autoCompact = false) =>
+	(autoCompact && n >= 100_000
+		? numberFormatterCompact
+		: numberFormatter
+	).format(n);
 
 export const sum = (values: number[]) =>
 	values.reduce((acc, next) => acc + next, 0);
