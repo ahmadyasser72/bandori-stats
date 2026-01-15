d5fcf35f9694cbc0c4837edb5893e31c36ed6a38
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-01-15 19:05:45 +0800

    also cache history route

--- site/src/middleware.ts
+++ site/src/middleware.ts
@@ -1,9 +1,13 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(
-	async ({ isPrerendered, url }, next) => {
+	async ({ isPrerendered, routePattern }, next) => {
 		const response = await next();
-		if (!isPrerendered && url.pathname.startsWith("/leaderboard")) {
+		if (
+			!isPrerendered &&
+			(routePattern.startsWith("/leaderboard") ||
+				routePattern.startsWith("/history"))
+		) {
 			response.headers.set(
 				"cache-control",
 				"max-age=60, stale-while-revalidate=3600",
