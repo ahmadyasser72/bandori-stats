891446c1415ace0b3293a31810320ddbf458c05a
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-02-02 21:34:31 +0800

    use search params for id instead of route params

--- site/src/lib/schema.ts
+++ site/src/lib/schema.ts
@@ -8,3 +8,4 @@ export const alwaysArray = <T extends z.ZodType>(schema: T) =>
 	schema.apply(maybeArray).transform((it) => (Array.isArray(it) ? it : [it]));
 
 export const dateSchema = z.iso.date().catch(dayjs.utc().format("YYYY-MM-DD"));
+export const idSchema = z.number().nonnegative();
