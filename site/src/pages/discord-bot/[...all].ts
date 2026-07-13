dc878347ac534c2b98b19c0fe4c52e3795d30d6c
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-07-13 23:38:16 +0800

    fix passing env/ctx to discord-bot (hono)

--- site/src/pages/discord-bot/[...all].ts
+++ site/src/pages/discord-bot/[...all].ts
@@ -1,12 +1,9 @@
 import { createHandler } from "@bandori-stats/discord-bot";
 
 import type { APIRoute } from "astro";
+import { env } from "cloudflare:workers";
 
 const handleDiscordInteractions = createHandler("/discord-bot");
 export const ALL: APIRoute = ({ request, locals }) => {
-	return handleDiscordInteractions.fetch(
-		request,
-		locals.runtime.env,
-		locals.runtime.ctx,
-	);
+	return handleDiscordInteractions.fetch(request, env, locals.cfContext);
 };
