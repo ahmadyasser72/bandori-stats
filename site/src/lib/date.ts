1e3ee5ec791ffa0f987307da8a0d4d18c1aec211
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-07-15 00:46:28 +0800

    update site to use game timezone like trigger.dev tasks

--- site/src/lib/date.ts
+++ site/src/lib/date.ts
@@ -1,10 +1,16 @@
+import { GBP_TIMEZONE } from "@bandori-stats/scheduled/constants";
+
 import dayjs from "dayjs";
 import quarterOfYear from "dayjs/plugin/quarterOfYear";
 import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 dayjs.extend(quarterOfYear);
+
+dayjs.tz.setDefault(GBP_TIMEZONE);
 
 export default dayjs;
--- site/src/lib/schema.ts
+++ site/src/lib/schema.ts
@@ -4,5 +4,7 @@ import dayjs from "~/lib/date";
 
 export const maybeArray = <T extends z.ZodType>(schema: T) =>
 	z.union([z.array(schema).nonempty(), schema]);
-export const dateSchema = z.iso.date().catch(dayjs.utc().format("YYYY-MM-DD"));
+export const dateSchema = z.iso
+	.date()
+	.catch(() => dayjs.tz().format("YYYY-MM-DD"));
 export const idSchema = z.coerce.number().nonnegative();
