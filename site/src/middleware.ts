d400267d1dcf5af1cc16a19ac4fe5efb3e581459
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-07-22 22:01:08 +0800

    ensure stable search queries across requests

--- site/src/middleware.ts
+++ site/src/middleware.ts
@@ -3,19 +3,37 @@ import dayjs from "@bandori-stats/bestdori/date";
 import { defineMiddleware } from "astro:middleware";
 import z from "zod";
 
-const querySchema = z.instanceof(URLSearchParams).transform((searchParams) =>
-	[...searchParams.entries()].reduce(
-		(acc, [key, value]) => {
-			if (value.length === 0) return acc;
+import { maybeArray } from "./lib/schema";
 
-			if (!(key in acc)) acc[key] = value;
-			else if (Array.isArray(acc[key])) acc[key].push(value);
-			else acc[key] = [acc[key], value];
+const querySchema = z.preprocess(
+	(input) => {
+		if (!(input instanceof URLSearchParams)) return {};
 
-			return acc;
-		},
-		{} as Record<string, string | string[]>,
-	),
+		const normalized = new URLSearchParams(
+			[...input.entries()].filter(([, value]) => value),
+		);
+		normalized.sort();
+
+		return {
+			normalized,
+			object: [...normalized.entries()].reduce(
+				(acc, [key, value]) => {
+					if (value.length === 0) return acc;
+
+					if (!(key in acc)) acc[key] = value;
+					else if (Array.isArray(acc[key])) acc[key].push(value);
+					else acc[key] = [acc[key], value];
+
+					return acc;
+				},
+				{} as Record<string, string | string[]>,
+			),
+		};
+	},
+	z.object({
+		object: z.record(z.string(), z.string().nonempty().apply(maybeArray)),
+		normalized: z.instanceof(URLSearchParams),
+	}),
 );
 
 export const onRequest = defineMiddleware(
@@ -23,12 +41,21 @@ export const onRequest = defineMiddleware(
 		const { data, error, success } = querySchema.safeParse(url.searchParams);
 		if (import.meta.env.DEV && !success)
 			throw new Error(z.prettifyError(error));
-		locals.parseQuery = (schema) => schema.parse(success ? data : {});
+		locals.parseQuery = (schema) => schema.parse(success ? data.object : {});
+
+		if (success && data.normalized.toString() !== url.search.slice(1))
+			return redirect(
+				data.normalized.size > 0
+					? `${url.pathname}?${data.normalized}`
+					: url.pathname,
+				302,
+			);
 
 		if (url.pathname.endsWith("/calendar") && !url.searchParams.has("date")) {
 			const search = new URLSearchParams(url.searchParams);
 			const thisMonth = dayjs.tz().startOf("month").format("YYYY-MM-DD");
 			search.set("date", thisMonth);
+			search.sort();
 			return redirect(`${url.pathname}?${search}`, 302);
 		}
 
