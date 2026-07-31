d690e615d29ef8024881a2767877291dc3ef98c2
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-07-31 21:35:12 +0800

    use rounder borders

--- site/src/styles/catppuccin.mocha.ts
+++ site/src/styles/catppuccin.mocha.ts
@@ -1,3 +1,11 @@
 import { createCatppuccinPlugin } from "@catppuccin/daisyui";
 
-export default createCatppuccinPlugin("mocha", {}, { prefersdark: true });
+export default createCatppuccinPlugin(
+	"mocha",
+	{
+		"--radius-box": "1rem",
+		"--radius-field": "0.5rem",
+		"--radius-selector": "1rem",
+	},
+	{ prefersdark: true },
+);
