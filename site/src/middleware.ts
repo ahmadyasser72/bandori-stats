7722d6e06157b97968559ad8cfd545164912d424
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-08-01 20:09:18 +0800

    rewrite instead of redirecting for normalizing query params

--- site/src/middleware.ts
+++ site/src/middleware.ts
@@ -37,18 +37,17 @@ const querySchema = z.preprocess(
 );
 
 export const onRequest = defineMiddleware(
-	async ({ locals, url, redirect }, next) => {
+	async ({ locals, url, rewrite }, next) => {
 		const { data, error, success } = querySchema.safeParse(url.searchParams);
 		if (import.meta.env.DEV && !success)
 			throw new Error(z.prettifyError(error));
 		locals.parseQuery = (schema) => schema.parse(success ? data.object : {});
 
 		if (success && data.normalized.toString() !== url.search.slice(1))
-			return redirect(
+			return rewrite(
 				data.normalized.size > 0
 					? `${url.pathname}?${data.normalized}`
 					: url.pathname,
-				302,
 			);
 
 		if (url.pathname.endsWith("/calendar") && !url.searchParams.has("date")) {
@@ -56,7 +55,7 @@ export const onRequest = defineMiddleware(
 			const thisMonth = dayjs.tz().startOf("month").format("YYYY-MM-DD");
 			search.set("date", thisMonth);
 			search.sort();
-			return redirect(`${url.pathname}?${search}`, 302);
+			return rewrite(`${url.pathname}?${search}`);
 		}
 
 		return next();
