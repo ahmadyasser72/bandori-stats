174d72794af47b319da0844de730fe0fef358460
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-09-05 12:42:45 +0800

    use latest nodejs on triggers

--- packages/triggers/trigger.config.ts
+++ packages/triggers/trigger.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
 	project: "proj_whqjzbgkgslcubigolcl",
-	runtime: "node",
+	runtime: "node-26",
 	logLevel: "debug",
 	maxDuration: 3600,
 	machine: "micro",
