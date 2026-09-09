a951f8ebb05ce931578b45f2d39a27ecf39ae24c
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-09-10 00:39:39 +0800

    add tracing to discord bot

--- packages/triggers/src/discord.ts
+++ packages/triggers/src/discord.ts
@@ -1,25 +1,30 @@
-import { AbortTaskRunError } from "@trigger.dev/sdk";
+import { AbortTaskRunError, logger } from "@trigger.dev/sdk";
 import { Client, type Guild } from "discord.js";
 import { limitAsync } from "es-toolkit";
 
 export const useDiscordBot = limitAsync(
 	async (
 		use: (value: { client: Client<true>; guild: Guild }) => Promise<void>,
-	) => {
-		const { DISCORD_BOT_TOKEN, DISCORD_GUILD_ID } = process.env;
-		if (!DISCORD_BOT_TOKEN || !DISCORD_GUILD_ID)
-			throw new AbortTaskRunError("Discord credentials are missing.");
+	) =>
+		logger.trace("discord-bot", async (span) => {
+			{
+				const { DISCORD_BOT_TOKEN, DISCORD_GUILD_ID } = process.env;
+				if (!DISCORD_BOT_TOKEN || !DISCORD_GUILD_ID)
+					throw new AbortTaskRunError("Discord credentials are missing.");
 
-		const client = new Client({ intents: [] });
-		await client.login(DISCORD_BOT_TOKEN);
-		const guild = await client.guilds.fetch(DISCORD_GUILD_ID);
-		await new Promise((resolve) => {
-			client.on("clientReady", (client) =>
-				use({ client, guild })
-					.finally(() => client.destroy())
-					.then(resolve),
-			);
-		});
-	},
+				const client = new Client({ intents: [] });
+				await client.login(DISCORD_BOT_TOKEN);
+				const guild = await client.guilds.fetch(DISCORD_GUILD_ID);
+				span.setAttribute?.("guild", guild.name);
+
+				await new Promise((resolve) => {
+					client.on("clientReady", (client) =>
+						use({ client, guild }).finally(() =>
+							client.destroy().then(resolve),
+						),
+					);
+				});
+			}
+		}),
 	1,
 );
